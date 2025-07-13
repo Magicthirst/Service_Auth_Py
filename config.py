f9fe1ae3cba1f7a7305c9876f6bf39386cc8cd8f
yoloroy <slart2108@gmail.com>
2025-07-13 21:52:21 +0300

    :construction_worker: Dockerize

--- config.py
+++ config.py
@@ -9,13 +9,17 @@ def generate_key():
     return base64.urlsafe_b64encode(os.urandom(32)).decode('utf-8')
 
 
+def not_found_env(variable_name):
+    raise Exception(f'not found env: {variable_name=}')
+
+
 protocol = 'http'
-host = os.environ.get("IP") or '0.0.0.0'
-port = os.environ.get("PORT") or 8000
+host = os.environ.get("IP") or not_found_env()
+port = int(os.environ.get("PORT")) or not_found_env()
 
 run_url = f'{protocol}://{host}:{port}'
 
-db_path = os.environ.get("DB") or 'users.db'
+db_path = os.environ.get("DB") or not_found_env()
 
 auth_key = os.environ.get("AUTH_KEY") or generate_key()
 auth_algorithm = 'HS256'
